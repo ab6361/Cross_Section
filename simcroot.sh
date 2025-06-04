da0c35271f523a1d77de5315b356dd45e2af17c4
Abhyuday Sharda <117782573+ab6361@users.noreply.github.com>
2025-06-04 13:09:27 -0400

    Update simcroot.sh
    Added executable

--- simcroot.sh
+++ simcroot.sh
@@ -8,12 +8,14 @@ if [ "$#" -ne 1 ]; then
 fi
 # The $0 variable represents the name of the script itself. fi ends the if block
 FILENAME="$1"
+# |: Takes the output of the command on the left and uses it as the input for the command on the right
+# Runs the executable simc and provides the infile name
+echo "$FILENAME" | ./simc
 
 # The || operator is a logical OR operator in Bash. It is used here to handle the case where the cd command fails
 # The exit 1 command will terminate the script with an exit status of 1, which is a common convention to indicate that an error has occurred
 cd util/ntuple/ || { echo "Directory util/ntuple/ not found."; exit 1; }
 
-# |: Takes the output of the command on the left and uses it as the input for the command on the right
 echo "$FILENAME" | ./make_ntuple
 cd ../../worksim || { echo "Directory worksim not found."; exit 1; }
 
